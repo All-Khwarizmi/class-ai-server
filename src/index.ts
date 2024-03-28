e17b3c40c8e80257eb5f0fc8c4ba274c4cb692db
Jason Suarez <swarecito@gmail.com>
2024-03-28 18:07:55 +0100

    Set simple express endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
+import dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import { Request, Response } from 'express'
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`)
+})
+
+
+
 console.log(process.env.PORT)
